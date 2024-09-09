aa58ae37f9639e58b78ebec9e8a9f86ce145628e
LinJHS <2360342887@qq.com>
2024-09-09 09:18:13 +0800

    Init main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import "./style.css";
 
-createApp(App).mount("#app");
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
