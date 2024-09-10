2247be94d904c1d2176b2458ead410dae593f681
LinJHS <2360342887@qq.com>
2024-09-10 22:49:23 +0800

    Add login util (axios)

---
+++ src/utils/login.js
@@ -0,0 +1,33 @@
+// 定义登录和注册的工具函数
+
+// 导入Axios库
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_BASE_URL + "/api/login";
+const timeout = 10000;
+var headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+};
+
+// 创建 Axios 实例
+var request = axios.create({
+  baseURL: baseURL,
+  timeout: timeout,
+  headers: headers,
+});
+
+// 登录
+const login = async (requestData) => {
+  let res = await request.post("", requestData);
+  return res;
+};
+
+// 注册
+const register = async (requestData) => {
+  let res = await request.put("", requestData);
+  return res;
+};
+
+// 导出函数
+export { login, register };
