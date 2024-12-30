64718636b0bccb71445969218d979b1b07f4555e
Momoyeyu <momoyeyu@outlook.com>
2024-12-30 17:17:37 +0800

    Add i18n into main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import "./style.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import i18n from "./i18n";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
