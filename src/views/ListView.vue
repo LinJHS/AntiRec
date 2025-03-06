74a0b6ae6d2f26a8c223202bde66b2f52df734f3
LinJHS <2360342887@qq.com>
2025-03-06 23:56:02 +0800

    Adjust the spacing of the scrollbars in the list on the left.

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -477,7 +477,7 @@ const btnReturn = () => {
 
   .record-list {
     margin: 10px;
-    padding-right: 10px;  /* 添加右侧内边距，为滚动条留出空间 */
+    padding-right: 6px;  /* 减小右侧内边距 */
     flex: 1;
     /* 占据剩余空间 */
     overflow-y: auto;
@@ -505,7 +505,7 @@ const btnReturn = () => {
     }
 
     .record-item {
-      margin-right: 4px;  /* 列表项右侧也留出一点间距 */
+      margin-right: 2px;  /* 减小列表项右侧间距 */
       padding: 8px 15px;
       border: 1px solid #ec8c8933;
       line-height: 30px;
